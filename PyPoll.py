1a1b2cec086d758f31d22c121ba927617933e961
alicegcr <alicegcr@gmail.com>
2022-09-10 11:23:01 -0700

    Adding the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -80,48 +80,60 @@ with open(file_to_load) as election_data:
         # Add a vote to the candidate's count 
         candidate_votes[candidate_name] += 1
 
-#Determine the percentage of votes for each candidate by looping through he counts:
-# 1. iterate through the candidate list:
-for candidate_name in candidate_votes:
-    #2. Retrieve vote count and percentage
-    votes = candidate_votes[candidate_name]
-    vote_percentage = float(votes) / float(total_votes) * 100
-    # To do: print out each candidate's name, vote count, percentage of
-    # votes to the terminal 
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:    
+    #Print the final vote count to the terminal 
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+    #Determine the percentage of votes for each candidate by looping through he counts:
+    # 1. iterate through the candidate list:
+    for candidate_name in candidate_votes:
+        #2. Retrieve vote count and percentage
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results =(
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine winning vote count and candidate 
-    # 1.Determine if the votes is greater than the winning count 
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # 2. If true then set the winning count = vote and winning percentage = 
-        # vote percentage 
-        winning_count = votes
-        winning_percentage = vote_percentage
-        # And, set the winning_candidate equal to the candidate's name 
-        winning_candidate = candidate_name
-# Print the winning candidate's results to the terminal
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning vote count: {winning_count:,}\n"
-    f"Winning percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
 
-print(winning_candidate_summary)
+        # Print each candidate's voter count and percentage to the terminal 
+        print(candidate_results)
+        # Save the candidate's results to the text file 
+        txt_file.write(candidate_results)
+        # Determine winning vote count and candidate 
+        # 1.Determine if the votes is greater than the winning count 
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # 2. If true then set the winning count = vote and winning percentage = 
+            # vote percentage 
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name 
+            winning_candidate = candidate_name
+    # Print the winning candidate's results to the terminal
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning vote count: {winning_count:,}\n"
+        f"Winning percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    # Save the winning candidate's results to the text file
+    txt_file.write(winning_candidate_summary)
 
       
-            #4. Print the candidate name and percentage of votes
-            #print(f'{candidate_name}: received {vote_percentage}% of the vote.')
-
-
-# 3. Print the total votes
-#print(candidate_votes)
+            
 
 
 
-    # # Print each row to csv file 
-    # for row in file_reader:
-    #     print(row)
+
+
+
+ 
 
    
 
